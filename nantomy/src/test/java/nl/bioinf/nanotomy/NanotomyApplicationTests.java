2459daf78d41032338ec5102dd0d7990394e8a35
Jelle <j.d.becirspahic@st.hanze.nl>
2021-03-04 15:33:20 +0100

    First intellij project commit

---
+++ nantomy/src/test/java/nl/bioinf/nanotomy/NanotomyApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.bioinf.nanotomy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NanotomyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
